acdc6ff4808eba1e82dd04e30e991920232eb6eb
bb84729 <bb84729@gmail.com>
2024-05-13 16:34:16 +0800

    aa

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("products/", include("store.urls")),
+    path("cart/", include("cart.urls")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
