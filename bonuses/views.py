c89fc24f2fc52a59fd94a849ef20e08acc02a7a4
Chien Chuan Wang <chien@chienchuan.com>
2024-06-04 12:54:32 +0800

    [modified] Added add_bonus and view_bonus functions

--- bonuses/views.py
+++ bonuses/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 
+@login_required
 def add_bonus(request):
-    pass
+    if request.method == "POST":
+        points = int(request.POST.get("points"))
+        bonus_point, created = Bonus.objects.get_or_create(user=request.user)
+        bonus_point.points += points
+        bonus_point.save()
+        return redirect("bonus_points")
+    return render(request, "bonuses/add_bonus_points.html")
 
 
+@login_required
 def view_bonus(request):
-    pass
+    bonus_points = Bonus.objects.filter(user=request.user).first()
+    return render(
+        request, "bonuses/view_bonus_points.html", {"bonus_points": bonus_points}
+    )
