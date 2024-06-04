175e54d5075572d2c0a23e97194aebb26b5ba621
Chien Chuan Wang <chien@chienchuan.com>
2024-06-04 12:53:52 +0800

    [modified] Created Bonus model

--- bonuses/models.py
+++ bonuses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Bonus(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+    earned_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.points} points"
